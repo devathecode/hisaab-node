fadb6c0bbdff3c31993ee41d40ea2dbd86fa1d07
Devanshu <devanshuv1501@gmail.com>
2023-03-10 02:31:54 +0530

    Third commit

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json());
 
-app.use(cors());
-app.use(express.json());
+let data = "";
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+// GET endpoint to retrieve data
+app.get("/api/data", (req, res) => {
+    res.send(data);
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+// POST endpoint to update data
+app.post("/api/data", (req, res) => {
+    data = req.body.data;
+    res.send("Data updated successfully.");
 });
+
+// Start the server
+app.listen(process.env.PORT || 5000, () =>
+    console.log(`Server running on port ${process.env.PORT || 5000}`)
+);
